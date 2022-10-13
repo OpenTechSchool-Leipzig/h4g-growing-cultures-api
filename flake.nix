65d064debf162da4b9d0cda2220941e450e28882
Dominik Kellner <dkellner@dkellner.de>
2022-10-13 20:42:05 +0200

    Set up Nix and direnv

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            cargo-expand
+            just
+            rust-analyzer
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
